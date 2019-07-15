15e699657571139edb11a544176c39d3ce362b7a
cdorado <carlosa.dorado@s-dos.es>
2019-07-15 17:31:55 +0200

    Clase4

---
+++ app/src/main/java/carlosa/melerosa/sdosa/atajosteclado/Clase4.kt
@@ -0,0 +1,10 @@
+package carlosa.melerosa.sdosa.atajosteclado
+
+class Clase4(val interfazMaldita: InterfazMaldita, val perdidaDeLaManoDeDios: InterfazPerdidaDeLaManoDeDios) {
+
+
+ fun test(){
+     interfazMaldita.metodoMaldito()
+     perdidaDeLaManoDeDios.metodoDeInterfaz()
+ }
+}
