a69c815a7acbf573b66171c38f6cbde6cb5a39ee
cdorado <carlosa.dorado@s-dos.es>
2019-07-15 17:43:00 +0200

    Clase4

---
+++ app/src/main/java/carlosa/melerosa/sdosa/atajosteclado/ClaseQueBuscadoraDeImplementacion.kt
@@ -0,0 +1,11 @@
+package carlosa.melerosa.sdosa.atajosteclado
+
+class ClaseQueBuscadoraDeImplementacion(val interfazMaldita: InterfazMaldita, val perdidaDeLaManoDeDios: InterfazPerdidaDeLaManoDeDios) {
+
+
+ fun test(){
+     //Prueba la diferencia entre Ctrl + click en el método y Ctrl + Alt + B
+     interfazMaldita.metodoMaldito()
+     perdidaDeLaManoDeDios.metodoDeInterfaz()
+ }
+}
